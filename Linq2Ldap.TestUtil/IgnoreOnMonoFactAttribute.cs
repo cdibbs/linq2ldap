d16d76a1e4e8a6fc86aa2152c4436a7ba070a96f
chris_dibbern <chrisdibbern@gmail.com>
2018-10-14 14:23:38 -0500

    Making platform-specific tests run accordingly

---
+++ Linq2Ldap.TestUtil/IgnoreOnMonoFactAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace Linq2Ldap.TestUtil
+{
+    // https://stackoverflow.com/questions/4421328/how-do-i-skip-specific-tests-in-xunit-based-on-current-platform
+    public class IgnoreOnMonoFactAttribute : FactAttribute {
+
+        public IgnoreOnMonoFactAttribute() {
+            if(IsRunningOnMono()) {
+                Skip = "Ignored on Mono";
+            }
+        }
+        /// <summary>
+        /// Determine if runtime is Mono.
+        /// Taken from http://stackoverflow.com/questions/721161
+        /// </summary>
+        /// <returns>True if being executed in Mono, false otherwise.</returns>
+        public static bool IsRunningOnMono() {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+    }
+}
--- Linq2Ldap.Tests/Proxies/DirectorySearcherProxyTests.cs
+++ Linq2Ldap.Tests/Proxies/DirectorySearcherProxyTests.cs
@@ -1,11 +1,12 @@
 using System.DirectoryServices;
 using Linq2Ldap.Proxies;
 using Xunit;
+using Linq2Ldap.TestUtil;
 
 namespace Linq2Ldap.Tests.Proxies {
     public class DirectorySearcherProxyTests {
 
-        [Fact]
+        [IgnoreOnMonoFact]
         public void Constructs_PassesBaseEntry() {
             var entry = new DirectoryEntry();
             var p = new DirectorySearcherProxy(entry);
