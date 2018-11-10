37f830f29c220550eb68f0eeb9a4834030ab3f49
chris_dibbern <chrisdibbern@gmail.com>
2018-11-10 17:29:35 -0600

    Fixing references

--- Linq2Ldap.Core.Tests/ModelCreatorTests.cs
+++ Linq2Ldap.Core.Tests/ModelCreatorTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AutoMapper;
-using Linq2Ldap.Core;
+﻿using System.Collections.Generic;
 using Linq2Ldap.Core.Attributes;
 using Linq2Ldap.Core.Models;
 using Linq2Ldap.Core.Proxies;
