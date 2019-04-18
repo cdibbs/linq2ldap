cff2cbb600fe962af684d852e5af19b7a1166cb5
Chris Dibbern <christopher-dibbern@uiowa.edu>
2019-04-18 15:32:10 -0500

    Adding two more protocols wrapper methods.

--- Linq2Ldap.Protocols/ILinq2LdapConnection.cs
+++ Linq2Ldap.Protocols/ILinq2LdapConnection.cs
@@ -1,7 +1,9 @@
 ﻿using Linq2Ldap.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
 using System.Text;
+using Linq2Ldap.Protocols.Proxies;
 
 namespace Linq2Ldap.Protocols
 {
@@ -9,7 +11,7 @@ namespace Linq2Ldap.Protocols
     /// A wrapper around the native Protocols connection object that facilitates sending
     /// LinqSearchRequest&lt;T&gt; requests.
     /// </summary>
-    public interface ILinq2LdapConnection
+    public interface ILinq2LdapConnection: ILdapConnectionProxy
     {
         /// <summary>
         /// Send a wrapped LINQ Expression-based request for a wrapped response that 
@@ -19,6 +21,28 @@ namespace Linq2Ldap.Protocols
         /// <param name="request">The LINQ search request definition.</param>
         /// <returns>A wrapped response with entries mapped to IEntry objects of type T.</returns>
         ILinqSearchResponse<T> SendRequest<T>(LinqSearchRequest<T> request)
+            where T : IEntry, new();
+
+        /// <summary>
+        /// Send a wrapped LINQ Expression-based request for a wrapped response that 
+        /// converts any found entries to objects of type T.
+        /// </summary>
+        /// <typeparam name="T">A class implementing IEntry to represent the found entries.</typeparam>
+        /// <param name="request">The LINQ search request definition.</param>
+        /// <param name="requestTimeout">The request timeout.</param>
+        /// <returns>A wrapped response with entries mapped to IEntry objects of type T.</returns>
+        ILinqSearchResponse<T> SendRequest<T>(
+            LinqSearchRequest<T> request,
+            TimeSpan requestTimeout)
+            where T : IEntry, new();
+
+        /// <summary>
+        /// Completes an asynchronous request.
+        /// To be used with BeginSearchRequest(DirectoryRequest, ...).
+        /// </summary>
+        /// <param name="asyncResult">Contains state data for this request.</param>
+        /// <returns>A wrapped, typed search response.</returns>
+        ILinqSearchResponse<T> EndSendRequest<T>(IAsyncResult asyncResult)
             where T : IEntry, new();
     }
 }
--- Linq2Ldap.Protocols/Proxies/ILdapConnectionProxy.cs
+++ Linq2Ldap.Protocols/Proxies/ILdapConnectionProxy.cs
@@ -1,9 +1,34 @@
-﻿using System.DirectoryServices.Protocols;
+﻿using System;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Linq2Ldap.Protocols.Proxies
 {
     public interface ILdapConnectionProxy
     {
+        AuthType AuthType { get; set; }
+
+        /// <summary>
+        /// Will bind on first search.
+        /// </summary>
+        bool AutoBind { get; set; }
+        NetworkCredential Credential { set; }
+        LdapSessionOptions SessionOptions { get; }
+        TimeSpan Timeout { get; set; }
+        X509CertificateCollection ClientCertificates { get; }
+        DirectoryIdentifier Directory { get; }
+
+        void Abort(IAsyncResult asyncResult);
+        IAsyncResult BeginSendRequest(DirectoryRequest request, PartialResultProcessing partialMode, AsyncCallback callback, object state);
+        IAsyncResult BeginSendRequest(DirectoryRequest request, TimeSpan requestTimeout, PartialResultProcessing partialMode, AsyncCallback callback, object state);
+        void Bind();
+        void Bind(NetworkCredential newCredential);
+        void Dispose();
+        void Dispose(bool disposing);
+        DirectoryResponse EndSendRequest(IAsyncResult asyncResult);
+        PartialResultsCollection GetPartialResults(IAsyncResult asyncResult);
         DirectoryResponse SendRequest(DirectoryRequest request);
+        DirectoryResponse SendRequest(DirectoryRequest request, TimeSpan requestTimeout);
     }
 }
